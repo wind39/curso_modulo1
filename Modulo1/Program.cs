98b71d1d8f09751aeee5272ddd01ee90400afc83
William Ivanski <william.ivanski@gmail.com>
2015-11-16 16:39:24 -0200

    Limpando codigo Modulo1

--- Modulo1/Program.cs
+++ Modulo1/Program.cs
@@ -7,24 +7,7 @@ namespace Modulo1
     {
         public static void Main(string[] args)
         {
-            List<int> vetor;
-            int n;
-
-            vetor = new List<int>();
-
-            Console.Write("Quantos números? ");
-            n = int.Parse(Console.ReadLine());
-
-            for (int i = 1; i <= n; i++)
-            {
-                Console.Write("Digite o número {0}: ", i);
-                vetor.Add(int.Parse(Console.ReadLine()));
-            }
-
-            Console.WriteLine("Os números que você digitou, na ordem inversa, são: ");
-
-            for (int i = vetor.Count-1; i >= 0; i--)
-                Console.WriteLine(vetor[i]);
+            
 
             Console.ReadKey();
         }
