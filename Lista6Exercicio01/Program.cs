bb46cb539834ba686bf7fd907ba321cbc9c88030
William Ivanski <william.ivanski@gmail.com>
2015-11-30 09:13:00 -0200

    M1 C6 P2

---
+++ Lista6Exercicio01/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Lista6Exercicio01
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // matriz real A
+            double[,] a;
+            // vetor real V
+            double[] v;
+            // m: número de linhas de A
+            // n: número de colunas de A, e número de elementos de V
+            int m, n;
+            // vetor real P, que é o produto de A por V
+            double[] p;
+
+            // dimensões
+            Console.Write("Digite o valor de M, que será o número de linhas de A: ");
+            m = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor de N, que será o número de colunas de A e número de elementos de V: ");
+            n = int.Parse(Console.ReadLine());
+
+            // alocando as matrizes e o vetor
+            a = new double[m,n];
+            v = new double[n];
+
+            // definicao do tamanho de P
+            // regra geral: Amxp . Bpxn = Cmxn
+            // Amxn . Vnx1 = Pmx1
+
+            // alocando o vetor resultante p
+            p = new double[m];
+
+            // solicitando ao usuário para digitar os elementos da matriz A
+            Console.WriteLine();
+            Console.WriteLine("Digite os elementos de A:");
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("Digite o elemento [{0},{1}]: ", i, j);
+                    a[i, j] = double.Parse(Console.ReadLine());
+                }
+
+            // solicitando ao usuário para digitar os elementos do vetor V
+            Console.WriteLine();
+            Console.WriteLine("Digite os elementos de V:");
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write("Digite o elemento [{0}]: ", j);
+                v[j] = double.Parse(Console.ReadLine());
+            }
+
+            // multiplicacao
+            for (int i = 0; i < m; i++)
+            {
+                p[i] = 0;
+                for (int j = 0; j < n; j++)
+                    p[i] += a[i,j] * v[j];
+            }
+
+            // impressão do vetor resultante
+            Console.WriteLine();
+            Console.WriteLine("O vetor resultante P da multiplicação entre A e V ficou:");
+            for (int i = 0; i < m; i++)
+                Console.WriteLine("P[{0}] = {1}", i, p[i]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lista6Exercicio04/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Lista6Exercicio04
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // matriz real A
+            double[,] a;
+            // matriz real B
+            double[,] b;
+            // m: número de linhas de A
+            // n: número de colunas de A, e número de linhas de B
+            // p: número de colunas de B
+            int m, n, p;
+            // matriz real C, que é o produto de A por B
+            double[,] c;
+
+            // dimensões
+            Console.Write("Digite o valor de M, que será o número de linhas de A: ");
+            m = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor de N, que será o número de colunas de A e número de linhas de B: ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor de P, que será o número de colunas de B: ");
+            p = int.Parse(Console.ReadLine());
+
+            // alocando as matrizes
+            a = new double[m,n];
+            b = new double[n,p];
+
+            // definicao do tamanho de C
+            // Cmxp = Amxn . Bnxp
+
+            // alocando a matriz resultante C
+            c = new double[m,p];
+
+            // solicitando ao usuário para digitar os elementos da matriz A
+            Console.WriteLine();
+            Console.WriteLine("Digite os elementos de A:");
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("Digite o elemento [{0},{1}]: ", i, j);
+                    a[i, j] = double.Parse(Console.ReadLine());
+                }
+
+            // solicitando ao usuário para digitar os elementos da matriz B
+            Console.WriteLine();
+            Console.WriteLine("Digite os elementos de B:");
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < p; j++)
+                {
+                    Console.Write("Digite o elemento [{0},{1}]: ", i, j);
+                    b[i, j] = double.Parse(Console.ReadLine());
+                }
+
+            // multiplicacao
+            /* Código do Felipe
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < p; j++) {
+                for (int k = 0; k < n; k++) {
+                matrizP [i, j] += matrizA [i, k] * matrizB [k, j];
+                }
+                Console.WriteLine ("P[{0},{1}]: {2}",i, j, matrizP [i,j]);
+                }
+                }
+            */
+
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < p; j++)
+                {
+                    c[i,j] = 0;
+                    for (int k = 0; k < n; k++)
+                        c[i,j] += a[i,k] * b[k,j];
+                }       
+
+            // impressão da matriz resultante
+            Console.WriteLine();
+            Console.WriteLine("A matriz resultante C da multiplicação entre A e B ficou:");
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < p; j++)
+                    Console.WriteLine("C[{0},{1}] = {2}", i, j, c[i,j]);
+
+            Console.ReadKey();
+        }
+    }
+}
